479be5cb9d5a1820bcbbeac78c6e50bcd8ef7b3a
sahmirzeli1806 <sahmirzeli.huseynov@gmail.com>
2020-07-04 22:41:14 +0400

    4july_22:42

--- lib/modes.dart
+++ lib/modes.dart
@@ -1,67 +1,12 @@
+import 'package:aquapro/aquaproicon_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class Modes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: 25,left: 10),
-                  height: 100,
-                  width: 100,
-                  color: Colors.grey[50],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Text("Əl yuma"),
-                      Container(
-                        height: 50,
-                        width: 50,
-                        margin: EdgeInsets.all(0.0),
-                        decoration: BoxDecoration(
-                            color: Colors.orange, shape: BoxShape.circle),
-                      ),
-                      Text("3 L"),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: 25,right: 10),
-                  height: 100,
-                  width: 100,
-                  color: Colors.grey[50],
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Text("Əl yuma"),
-                      Container(
-                        height: 50,
-                        width: 50,
-                        margin: EdgeInsets.all(0.0),
-                        decoration: BoxDecoration(
-                            color: Colors.orange, shape: BoxShape.circle),
-                      ),
-                      Text("3 L"),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -70,22 +15,52 @@ class Modes extends StatelessWidget {
               //mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 Container(
-                  margin: EdgeInsets.all(10),
-                  height: 250,
-                  width: 250,
+                  margin: EdgeInsets.only(top:37,bottom: 45),
+                  height: 300,
+                  width: 300,
                   color: Colors.grey[50],
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      Text("Əl yuma"),
                       Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1,color: Colors.lightBlue),
+                          boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(0.7, 4.0), //(x,y)
+                    blurRadius: 6.0,
+                  ),
+                ],
+                          // border: Border.all(width: 2),
+                          shape: BoxShape.circle,
+                          // You can use like this way or like the below line
+                          //borderRadius: new BorderRadius.circular(30.0),
+                          color: Colors.white,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('17.3 L',style: TextStyle(fontSize: 30,color: Colors.lightBlue)),
+                            
+                          ],
+                        ),
+                        margin: EdgeInsets.only(top :30,bottom: 30),
+                      ),
+                      /* Container(
+                        child: Icon(Aquaproicon.water,size: 100,color: Colors.blue)
+                      ),
+                       Container(
                         height: 100,
                         width: 100,
                         margin: EdgeInsets.all(0.0),
                         decoration: BoxDecoration(
                             color: Colors.orange, shape: BoxShape.circle),
-                      ),
-                      Text("3 L"),
+                      ), */
+                      Text("Ümumi istifadə"),
                     ],
                   ),
                 ),
@@ -102,21 +77,34 @@ class Modes extends StatelessWidget {
               children: <Widget>[
                 Container(
                   margin: EdgeInsets.all(10),
-                  height: 100,
-                  width: 100,
+                  height: 150,
+                  width: 150,
                   color: Colors.grey[50],
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
                       Text("Əl yuma"),
                       Container(
+                        child: Icon(
+                          MdiIcons.handWater,
+                          size: 60,
+                          color: Colors.lightBlue[400],
+
+                          
+                          ),
+                        
+                        ),
+
+                      /* Container(
                         height: 50,
                         width: 50,
                         margin: EdgeInsets.all(0.0),
                         decoration: BoxDecoration(
-                            color: Colors.orange, shape: BoxShape.circle),
-                      ),
-                      Text("3 L"),
+                          
+                        color: Colors.orange, shape: BoxShape.circle), 
+
+                      ), */
+                      Text("5 L"),
                     ],
                   ),
                 ),
--- lib/schedule.dart
+++ lib/schedule.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class Schedule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemExtent: 80,
-        itemCount: 10, 
+        itemExtent: 100,
+        itemCount: 10,
         itemBuilder: (context, index) {
-          return Container(
+          return Card(
+            elevation: 3,
+            child: ListTile(
+              //contentPadding: EdgeInsets.symmetric(horizontal: 0,0 ),
+              leading: Padding(padding: const EdgeInsets.only(top: 25),
+              child: Icon(MdiIcons.handWater,size: 37,),
+              
+               )
+            ),
+          );
+        });
+  }
+}
+
+/* 
+
+Container(
             padding: EdgeInsets.all(10),
             child: Material(
               elevation: 3,
@@ -15,6 +32,5 @@ class Schedule extends StatelessWidget {
               child: Center(child: Text(index.toString(),)),
             ),
           );
-        });
-  }
-}
+          
+*/
